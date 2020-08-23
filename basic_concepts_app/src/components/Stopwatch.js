d92ff5d8e65449aa398d6075ffdd986923d8a089
hunterkillzero <snguoila@yahoo.com>
2020-08-23 12:22:58 +0700

    Conditional rendering with if ... else instead of ternary operator

--- basic_concepts_app/src/components/Stopwatch.js
+++ basic_concepts_app/src/components/Stopwatch.js
@@ -8,21 +8,28 @@ class Stopwatch extends React.Component {
 
   handleStopwatch = () => {
     this.setState({
-        isRunning: !this.state.isRunning
+      isRunning: !this.state.isRunning
     });
   }
 
   render() {
-      return (
-        <div className="stopwatch">
-          <h2>Stopwatch</h2>
-          <span className="stopwatch-time">0</span>
-          <button onClick={this.handleStopwatch}>
-            { this.state.isRunning ? 'Stop' : 'Start' }
-          </button>
-          <button>Reset</button>
-        </div>
-      );
+    let button;
+    if (this.state.isRunning) {
+      button = <button>Stop</button>;
+      } else {
+        button = <button>Start</button>;
+    }
+    return (
+      <div className="stopwatch">
+        <h2>Stopwatch</h2>
+        <span className="stopwatch-time">3...</span>
+      // <button onClick={this.handleStopwatch}>
+      //   { this.state.isRunning ? 'Stop' : 'Start' }
+      // </button>
+        { button }
+      <button>Reset</button>
+    </div>
+  );
   }
 }
 
